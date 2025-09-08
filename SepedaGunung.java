6ff4c56a8eb782d19819feafc73139977408f256
ZainKautsar-doc <zainkautsarridha@gmail.com>
2025-09-08 14:36:54 +0700

    Konnstruktor sblm ini dan overloading dan override

--- SepedaGunung.java
+++ SepedaGunung.java
@@ -1,10 +1,6 @@
 public class SepedaGunung extends Sepeda {
-    private int pedal;
-    void setPedal(int jumlah) {
-        pedal = getGir() - jumlah;
-    }
-
-    int getPedal() {
-        return pedal;
+    void setGir(int pertambahanGir) {
+        super.setGir(pertambahanGir);
+        gir = 2 * gir;
     }
 }
--- SepedaGunungBeraksi.java
+++ SepedaGunungBeraksi.java
@@ -1,11 +1,11 @@
 public class SepedaGunungBeraksi {
     public static void main(String[] args) {
         SepedaGunung sg = new SepedaGunung();
-        sg.setGir(3);
-        System.out.println("Nilai awal = " + sg.getGir());
 
-        sg.setPedal(1);
-        System.out.println("Pedal setelah dikurangi 1 = " + sg.getPedal());
-    }
-    
+        sg.setGir(2);
+        System.out.println("Gir sepeda gunung: " + sg.getGir());
+
+        sg.setGir(3);
+        System.out.println("Gir sepeda gunung: " + sg.getGir());
+    }        
 }
