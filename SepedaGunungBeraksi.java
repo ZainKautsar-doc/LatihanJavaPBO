6420ee88804cfaf0c194a2773c1f67481423461f
ZainKautsar-doc <zainkautsarridha@gmail.com>
2025-09-08 13:55:05 +0700

    Ralat Latihan

--- SepedaGunungBeraksi.java
+++ SepedaGunungBeraksi.java
@@ -2,10 +2,10 @@ public class SepedaGunungBeraksi {
     public static void main(String[] args) {
         SepedaGunung sg = new SepedaGunung();
         sg.setGir(3);
-        System.out.println(sg.getGir());
+        System.out.println("Nilai awal = " + sg.getGir());
 
         sg.setPedal(1);
-        System.out.println("Pedal setelah dikurangi 1: " + sg.getPedal());
+        System.out.println("Pedal setelah dikurangi 1 = " + sg.getPedal());
     }
     
 }
