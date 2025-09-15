6da61a286932b36e67c13ee6f03223f8e461fb0a
ZainKautsar-doc <zainkautsarridha@gmail.com>
2025-09-15 13:56:47 +0700

    Import

---
+++ PaketBeraksi.java
@@ -0,0 +1,8 @@
+import kelasku.Joko;
+
+public class PaketBeraksi {
+    public static void main(String[] args) {
+        Joko objectJoko = new Joko();
+        objectJoko.info();
+    }
+}
---
+++ PaketBeraksi2.java
@@ -0,0 +1,10 @@
+import kelasku.*;
+
+public class PaketBeraksi2 {
+    public static void main(String[] args) {
+        Joko objectJoko = new Joko();
+        objectJoko.info();
+        Budi objectBudi = new Budi();
+        objectBudi.info();
+    }
+}
